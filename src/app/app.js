a9895c78fae4c557d1cd0644c1d001efcbc6c65e
uGhz <ghuzmush@gmail.com>
2016-06-13 21:01:35 +0200

    Récupération des links VIAF et affichage par un template Mustache.

--- src/app/app.js
+++ src/app/app.js
@@ -14,9 +14,9 @@ $('img').fadeIn(); // makes images disappear
 		urlRoot: 'http://viaf.org/viaf',
 		url: function() {
 			// return this.urlRoot + '/' + this.id + '/justlinks.json';
-			// return this.urlRoot + '/96994048/justlinks.json';
-			// return 'http://127.0.0.1:80/biography-enrichment/proxy.php?source=viaf-links&viaf-id=96994048';
-			return 'http://172.22.100.140/biography-enrichment/proxy.php?source=viaf-links&viaf-id=96994048';
+			// return this.urlRoot + '/96994048/justlinks.json';			
+			return 'http://127.0.0.1:80/biography-enrichment/proxy.php?source=viaf-links&viaf-id=96994048';
+			// return 'http://172.22.100.140/biography-enrichment/proxy.php?source=viaf-links&viaf-id=96994048';
 		},
 		parse: function(data) {
 			var result = [];
@@ -35,33 +35,33 @@ $('img').fadeIn(); // makes images disappear
 		
 			initialize: function() {
 				console.log("ViafLinksView INITIALIZE");
-				this.el = '#viaf-links-container';
+				this.setElement('#viaf-links-container');
 				var rawTemplate = $('#viaf-links-template').html();
-				console.log(rawTemplate);
+				// console.log(rawTemplate);
 				// this.template = Mustache.parse(rawTemplate);
 				this.template = rawTemplate;
-				this.collection.on('add', this.render, this);
+				// this.collection.on('add', this.render, this);
+				
+				this.collection.on('sync', this.updateView, this);
 				
 				// this.listenTo(this.model, 'change', this.render);
 			},
 			
+			updateView: function () {
+			    console.log('sync');
+			    this.render();
+			},
+			
 			render: function(){
 				console.log("ViafLinksView RENDER");
-				// console.log(this.collection);
-				// console.log(JSON.stringify(this.collection));
 				
-				/*
-				this.collection.each(function(person){
-			            console.log(person);
-				});
-				*/
-				console.log(_.isArray(this.template));
-				console.log(_.isArray({"viaf-links" : this.collection}));
-				
-				var renderData = JSON.stringify(this.collection);
+				// var renderData = {"viaf-links" : JSON.parse(JSON.stringify(this.collection)};
+				var renderData = {"viaf-links" : JSON.parse(JSON.stringify(this.collection))};
 				console.log(renderData);
+				var renderMarkup = Mustache.to_html(this.template, renderData);
+				console.log("Ready to update DOM.");
 				
-				this.$el.html(Mustache.to_html(this.template, renderData));
+				this.$el.html(renderMarkup);
 				
 			}
 		});
